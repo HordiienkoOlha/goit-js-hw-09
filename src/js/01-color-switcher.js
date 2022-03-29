bb34a5864d95fd850ba2675594ea95ca39a08ece
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-03-29 21:41:06 +0300

    edit files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,17 +3,26 @@ const btnStart = document.querySelector('button[data-start]');
 // console.log(btnStart);
 const btnStop = document.querySelector('button[data-stop]');
 // console.log(btnStop);
-btnStart.addEventListener(
-  'click',
-  () => {
-    bodyColor.style.backgroundColor = getRandomHexColor();
-  },
-  //   { once: true },
-);
+
+// const intervalId = setInterval(logger, 1000, 2000);
+
+btnStart.addEventListener('click', () => {
+  const logger = () => (bodyColor.style.backgroundColor = getRandomHexColor());
+  setInterval(logger, 1000, 1000);
+});
 btnStop.addEventListener('click', () => {
-  bodyColor.style.backgroundColor = '#fafafa';
+  // bodyColor.style.backgroundColor = '#fafafa';
+  // const intervalId = setInterval(logger, 1000, 2000),
+  // clearInterval(logger),
+  console.log('stop');
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// const timerId = setInterval(() => {
+//  console.log(' ');
+// }, 2000);
+// setTimeout(() => {
+clearInterval(timerId);
+// }, 5000);
