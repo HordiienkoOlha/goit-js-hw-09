cdade3b51571e497388cfbfe8d0561429b21ca27
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-04-01 21:26:40 +0300

    done task - 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,24 @@
 const bodyColor = document.querySelector('body');
 const btnStart = document.querySelector('button[data-start]');
-// console.log(btnStart);
 const btnStop = document.querySelector('button[data-stop]');
-// console.log(btnStop);
-
-// const intervalId = setInterval(logger, 1000, 2000);
+let logger = null;
 
 btnStart.addEventListener('click', () => {
-  const logger = () => (bodyColor.style.backgroundColor = getRandomHexColor());
-  setInterval(logger, 1000, 1000);
+  logger = setInterval(
+    () => {
+      bodyColor.style.backgroundColor = getRandomHexColor();
+    },
+    1000,
+    1000,
+  );
+  btnStart.setAttribute('disabled', true);
 });
+
 btnStop.addEventListener('click', () => {
-  // bodyColor.style.backgroundColor = '#fafafa';
-  // const intervalId = setInterval(logger, 1000, 2000),
-  // clearInterval(logger),
-  console.log('stop');
+  clearInterval(logger);
+  btnStart.removeAttribute('disabled');
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// const timerId = setInterval(() => {
-//  console.log(' ');
-// }, 2000);
-// setTimeout(() => {
-clearInterval(timerId);
-// }, 5000);
