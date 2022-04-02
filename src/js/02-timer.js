136ff6448029048e66e519e3d9e1defdef3aadbe
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-04-02 15:25:42 +0300

    edit task- 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,8 +4,14 @@ import 'flatpickr/dist/flatpickr.min.css';
 // import { uk } from 'date-fns/locale';
 const calendarInput = document.querySelector('#datetime-picker');
 const buttonStart = document.querySelector('button');
-
-// console.log(dateNow);
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+// console.log(dataDays);
+// console.log(dataHours);
+// console.log(dataMinutes);
+// console.log(dataSeconds);
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -18,6 +24,7 @@ const options = {
     buttonStart.removeAttribute('disabled');
     if (selectedDates[0] < Date.now()) {
       window.alert('Please choose a date in the future');
+      buttonStart.setAttribute('disabled', true);
     }
     console.log(selectedDates[0]);
     console.log(Date.now());
@@ -28,10 +35,25 @@ const fp = flatpickr(calendarInput, options);
 buttonStart.addEventListener('click', () => {
   console.log('клик');
 });
-// const output = document.querySelector('.output');
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-// calendarInput.addEventListener('input', event => {
-//   const userInput = event.currentTarget.value;
-//   //   console.log('input');
-//   console.log(userInput);
-// });
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
